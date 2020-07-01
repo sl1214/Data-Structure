1195627778da70b6e9b44b0a8153dc453bad01b5
sl1214 <894654642@qq.com>
2020-07-01 18:22:22 +0800

    新增个别方法

--- src/LinkList.java
+++ src/LinkList.java
@@ -18,7 +18,7 @@ public class LinkList<T> extends AbsList<T> implements Iterator<T> {
 
     protected Lnode<T> getNode(int i) {
         Lnode<T> x = null;
-        for (x = first; x != null && i > 0 && i<length; i--) {
+        for (x = first; x != null && i >= 0 && i < length; i--) {
             x = x.next;
         }
         return x;
@@ -27,7 +27,7 @@ public class LinkList<T> extends AbsList<T> implements Iterator<T> {
     @Override
     public T get(int i) {
         Lnode<T> p = getNode(i);
-        if ( p == null) {
+        if (p == null) {
             return null;
         } else {
             return p.data;
@@ -37,7 +37,7 @@ public class LinkList<T> extends AbsList<T> implements Iterator<T> {
     @Override
     public T set(int index, T element) {
         Lnode<T> p = getNode(index);
-        if ( p == null) {
+        if (p == null) {
             return null;
         } else {
             T oldData = p.data;
@@ -48,37 +48,137 @@ public class LinkList<T> extends AbsList<T> implements Iterator<T> {
 
     @Override
     public int indexOf(int begin, int end, Object x) {
-        return 0;
+        Lnode<T> node = getNode(begin);
+        while (begin <= end && node != null) {
+            if (x == null && node.data == null) {
+                return begin;
+            }
+            if (node.data.equals(x)) {
+                return begin;
+            }
+            begin++;
+            node = node.next;
+        }
+        return -1;
     }
 
     @Override
     public void add(int i, T x) {
+        Lnode<T> s;
+        s = new Lnode<T>(x, null);
+        if (first == null || length == 0) {
+            first = s;
+            last = s;
+        } else if (i < 1) {
+            s.next = first;
+            first = s;
+        } else if (i >= length) {
+            last.next = s;
+            last = s;
+        } else {
+            Lnode<T> p = getNode(i - 1);
+            s.next = p.next;
+            p.next = s;
+        }
+        length++;
+    }
 
+    public boolean add(T key) {
+        this.add(length, key);
+        return true;
     }
 
     public void addBack(T x) {
-
+        this.add(length, x);
     }
 
     public void Front(T x) {
+        this.add(0, x);
+    }
 
+    public void insertBefore(Lnode<T> first, Lnode<T> p, T x) {
+        Lnode<T> lnode = new Lnode<T>(x);
+        if (p == first) {
+            lnode.next = p;
+            this.first = lnode;
+        } else {
+            Lnode<T> q = first;
+            while (q.next != p) {
+                q = q.next;
+            }
+            lnode.next = p;
+            q.next = lnode;
+        }
     }
 
     public void sort() {
+        if (first == null) {
+            return;
+        } else {
+            Lnode<T> node = first.next;
+            first.next = null;
+            while (node != null) {
+                Lnode<T> s = node.next;
+                insertOrder(node);
+                node = s;
+            }
+
+        }
     }
 
-    public void addSort() {
+    public void addSort(T e) {
+        Lnode<T> s = new Lnode<T>(e);
+        this.insertOrder(s);
     }
 
     private void insertOrder(Lnode<T> s) {
-
+        if (first == null) {
+            first = s;
+        } else if (s.compareTo(first) <= 0) {
+            s.next = first;
+            first = s;
+        } else if (s.compareTo(last) >= 0) {
+            last.next = s;
+            last = s;
+        } else {
+            Lnode<T> node = first;
+            while (node != null) {
+                if (node.compareTo(s) <= 0 && node.next.compareTo(s) >= 0) {
+                    s.next = node.next;
+                    node.next = s;
+                    break;
+                }
+                node = node.next;
+            }
+        }
     }
 
     protected Lnode<T> removeNode(int i) {
-        return null;
+        Lnode<T> q;
+        if (i == 0) {
+            q = first;
+            first = first.next;
+        } else {
+            Lnode<T> p = getNode(i - 1);
+            q = p.next;
+            p.next = p.next.next;
+        }
+        return q;
     }
 
     public T search(T key) {
+        Lnode<T> node = getNode(0);
+        int i = 0;
+        while (i <= length - 1 && node != null) {
+            if (key == null && node.data == null) {
+                return key;
+            }
+            if (node.data.equals(key)) {
+                return key;
+            }
+            i++;
+            node = node.next;
+        }
         return null;
     }
 
@@ -90,7 +190,7 @@ public class LinkList<T> extends AbsList<T> implements Iterator<T> {
 
     @Override
     public boolean contains(Object o) {
-        return false;
+        return indexOf(o) != -1;
     }
 
     @Override
@@ -105,6 +205,7 @@ public class LinkList<T> extends AbsList<T> implements Iterator<T> {
 
     @Override
     public boolean remove(Object o) {
+
         return false;
     }
 
